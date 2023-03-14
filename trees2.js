f10cebf5175760f64f3e0dd2838243397d533e5f
Melvin He <melvin_he@brown.edu>
2023-03-14 18:03:35 -0400

    trees

--- trees2.js
+++ trees2.js
@@ -31,36 +31,24 @@ function addTree2(jsonFeature) {
     item.setAttribute("class", "tree");
     item.setAttribute("data-reltilex", Math.floor(itemPos.x));
     item.setAttribute("data-reltiley", Math.floor(itemPos.y));
-    var trunk = document.createElement("a-entity");
-    trunk.setAttribute("class", "trunk");
     var crown = document.createElement("a-entity");
     crown.setAttribute("class", "crown");
     var height = tags.height ? tags.height : 8;
-    var trunkRadius = (tags.circumference ? tags.circumference : 1) / 2 / Math.PI;
     var crownRadius = (tags.diameter_crown ? tags.diameter_crown : 3) / 2;
     // leaf_type is broadleaved, needleleaved, mixed or rarely something else.
     if (tags["leaf_type"] == "needleleaved") { // special shape for needle-leaved trees
-      var trunkHeight = height * 0.5;
-      var crownHeight = height * 0.01;
-      trunk.setAttribute("geometry", {primitive: "cylinder", height: trunkHeight, radius: trunkRadius});
-      trunk.setAttribute("material", {color: "#b27f36"});
-      trunk.setAttribute("position", {x: 0, y: (trunkHeight / 2), z: 0});
+      var crownHeight = height * 0.8;
       crown.setAttribute("geometry", {primitive: "cone", height: crownHeight, radiusBottom: crownRadius, radiusTop: 0});
       crown.setAttribute("material", {color: "#80ff80"});
       crown.setAttribute("position", {x: 0, y: (height - crownHeight / 2), z: 0});
     }
     else { // use a simple typical broadleaved-type shape
-      var trunkHeight = height - crownRadius;
-      trunk.setAttribute("geometry", {primitive: "cylinder", height: trunkHeight, radius: trunkRadius});
-      trunk.setAttribute("material", {color: "#b27f36"});
-      trunk.setAttribute("position", {x: 0, y: (trunkHeight / 2), z: 0});
       crown.setAttribute("geometry", {primitive: "sphere", radius: crownRadius});
       crown.setAttribute("material", {color: "#80ff80"});
-      crown.setAttribute("position", {x: 0, y: trunkHeight, z: 0});
+      crown.setAttribute("position", {x: 0, y: 0, z: 0});
     }
     item.setAttribute("position", getPositionFromTilepos(itemPos));
     item.setAttribute("data-gpspos", jsonFeature.geometry.coordinates[1] + "/" + jsonFeature.geometry.coordinates[0]);
-    item.appendChild(trunk);
     item.appendChild(crown);
     items.appendChild(item);
     resolve();
